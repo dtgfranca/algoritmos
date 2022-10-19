022330a9585c9ca4a0692cd602457b465f5fe7e1
Diego França <diego.lightbase@sebraemg.com.br>
2022-10-18 21:43:17 -0300

    feat: implementa o bubblesort

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@
 require_once '/var/www/vendor/autoload.php';
 
 $bubbleSort = new \Diego\Algoritmos\BubbleSort\BubbleSort();
-print_r($bubbleSort->execute());
+print_r($bubbleSort->execute([10,7,4,11]));
 
--- src/BubbleSort/BubbleSort.php
+++ src/BubbleSort/BubbleSort.php
@@ -2,8 +2,21 @@
 namespace Diego\Algoritmos\BubbleSort;
 class BubbleSort
 {
-    public function execute(): array
+    public function execute(array $array): array
     {
-        return ['teset'];
+        for($i=count($array) -1; $i >0; $i--){
+            $change = 0;
+            for ($j=0; $j < $i; $j++) {
+                if ($array[$j] > $array[$j+1]) {
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j +1];
+                    $array[$j +1] = $temp;
+                    $change++;
+                }
+            }
+            if ($change <= 1) break;
+        }
+
+        return $array;
     }
 }
