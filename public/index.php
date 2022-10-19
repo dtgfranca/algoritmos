e2efae0b3289bb5ef5f80a449d5f4b90c0698b8c
Diego França <diego.lightbase@sebraemg.com.br>
2022-10-18 21:44:26 -0300

    feat: implementa o bubblesort

--- public/index.php
+++ public/index.php
@@ -3,5 +3,7 @@
 require_once '/var/www/vendor/autoload.php';
 
 $bubbleSort = new \Diego\Algoritmos\BubbleSort\BubbleSort();
+echo"####Bubble Sort###\n";
+echo "Ordenando o array [10,7,4,11]\n";
 print_r($bubbleSort->execute([10,7,4,11]));
-
+echo "#######\n";
